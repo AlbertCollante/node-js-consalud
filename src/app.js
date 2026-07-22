15a287843a92cd8e2abf6942924c851c39cad7ab
AlbertCollante <ackm.1124@gmail.com>
2026-07-21 22:31:29 -0500

    prueba de error 3

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { pool } from './db.js';
 import { PORT } from './config.js';
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from './config.js';
 import proveedoresRoutes from './routes/proveedores.routes.js';
 import pedidosRoutes from './routes/pedidos.routes.js';
 import usuariosRoutes from './routes/usuarios.routes.js';
